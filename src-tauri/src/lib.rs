121d6ffead573db17d04f5e3c4a47186641f0f4e
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-05-12 16:16:59 -0400

    Refactor command handling and integrate Excel support
    Removed the default `greet` command and replaced it with `get_sheet_names` from the `excel_commands` module. Streamlines functionality by focusing on Excel-related operations.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,17 +1,12 @@
 mod commands;
-
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
+use commands::excel_commands;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_shell::init())
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![excel_commands::get_sheet_names])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
