5b91328afb8e1a40023e307a87ab1187e90d2bd1
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-05-13 16:14:08 -0400

    Add loading state and enhance save/print functionalities
    Introduced a loading spinner when saving sheet data as PNGs and added proper sheet naming support. Updated print functionality to handle sheet data dynamically and ensured smoother animations during loading.

--- src/assets/pages/GenerationPage.tsx
+++ src/assets/pages/GenerationPage.tsx
@@ -3,30 +3,47 @@ import {useLocation, useNavigate} from "react-router-dom";
 import {ExcelCommands, SheetData} from "../ts/ExcelCommands.ts";
 import QRCode from "../components/QRCode.tsx";
 import {Icon} from "@iconify-icon/react";
-import {Button, cn} from "@heroui/react";
+import {Button, cn, Spinner} from "@heroui/react";
 import {motion} from "framer-motion";
+import {PrintCommands} from "../ts/PrintCommands.ts";
 
 export default function GenerationPage()
 {
     const navigate = useNavigate();
     const {search} = useLocation();
     const [data, setData] = useState<SheetData[][]>([]);
+    const [sheetNames, setSheetNames] = useState<string[]>([]);
+    const [isLoading, setIsLoading] = useState(false);
 
     useEffect(() =>
     {
-        ExcelCommands.extractSheetDataFromSearchParams(search, navigate).then(setData);
+        ExcelCommands
+            .extractSheetDataFromSearchParams(search, navigate)
+            .then(result => {
+                setData(result.data);
+                setSheetNames(result.sheetNames);
+            });
     }, [search]);
     if (data.length === 0) return null;
     return (
         <>
+            {isLoading &&
+                <div className={
+                    cn(
+                        "absolute inset-0 m-auto rounded-full w-[170px] h-16",
+                        "flex flex-row justify-center items-center gap-2 px-4"
+                    )}>
+                    <Spinner size={"lg"} label={"Saving..."}/>
+                </div>
+            }
             <motion.div
                 className={"flex flex-col overflow-auto ml-2"}
                 initial={{opacity: 0, y: 20}}
-                animate={{opacity: 1, y: 0}}
+                animate={{opacity: isLoading ? 0 : 1, y: isLoading ? 20 : 0}}
                 exit={{opacity: 0, y: 20}}
                 transition={{duration: 0.2, delay: 0.2, ease: "easeInOut"}}
             >
-                {data.map(sheetData => <QRSheet sheet={sheetData}/>)}
+                {data.map((sheetData, index) => <QRSheet key={index} sheet={sheetData}/>)}
             </motion.div>
             <motion.div
                 className={
@@ -38,12 +55,32 @@ export default function GenerationPage()
                 }
 
                 initial={{opacity: 0, y: 80}}
-                animate={{opacity: 1, y: 0}}
+                animate={{opacity: isLoading ? 0 : 1, y: isLoading ? 80 : 0}}
                 exit={{opacity: 0, y: 20}}
                 transition={{duration: 0.5, delay: .5, ease: "easeInOut"}}
             >
-                <Button color={"primary"} radius={"full"} className={"min-w-0 w-10"}><Icon icon={"mage:printer-fill"}/></Button>
-                <Button startContent={<Icon icon={"mage:save-floppy-fill"}/>} className={"text-black bg-white"} radius={"full"}>Save</Button>
+                <Button
+                    color={"primary"}
+                    radius={"full"}
+                    className={"min-w-0 w-10"}
+                    onPress={() => PrintCommands.printSheetData(data)}
+                >
+                    <Icon icon={"mage:printer-fill"}/>
+                </Button>
+                <Button
+                    startContent={<Icon icon={"mage:save-floppy-fill"}/>}
+                    className={"text-black bg-white"}
+                    radius={"full"}
+                    onPress={async () =>
+                    {
+                        setIsLoading(true);
+                        await PrintCommands
+                            .saveSheetDataAsPng(data, sheetNames);
+                        setIsLoading(false);
+                    }}
+                >
+                    Save
+                </Button>
             </motion.div>
         </>
     );
