df33d9337e6093864cf3bc8797d984c219560679
Drew Chase (Work) <dcmanproductions@gmail.com>
2025-05-13 09:29:57 -0400

    Add error handling and navigation for missing file path
    Introduced a toast notification to alert users when navigating to the Select Sheet page without a file path. Updated `onSheetSelected` to properly navigate with file path and selected sheets encoded in the URL.

--- src/assets/pages/SelectSheetPage.tsx
+++ src/assets/pages/SelectSheetPage.tsx
@@ -1,6 +1,7 @@
 import SheetSelector from "../components/SheetSelector.tsx";
 import {useLocation, useNavigate} from "react-router-dom";
 import {useEffect, useState} from "react";
+import {addToast} from "@heroui/react";
 
 export default function SelectSheetPage()
 {
@@ -12,15 +13,29 @@ export default function SelectSheetPage()
     {
         const params = new URLSearchParams(search);
         const filePath = params.get("file");
-        if (filePath === null) navigate("/");
-        else setFilePath(decodeURIComponent(filePath));
+        if (filePath === null)
+        {
+            addToast({
+                title: "Unexpected Route Change",
+                description: "A transfer to the Select Sheet page was triggered without a file path. Please report this issue to the developer.",
+                color: "danger"
+            });
+            navigate("/");
+        } else setFilePath(decodeURIComponent(filePath));
     }, [search]);
 
     if (filePath === undefined) return null;
 
     return (
-        <SheetSelector filePath={filePath} onSheetSelected={_sheets =>
-        {
-        }} onCancel={() => navigate("/")}/>
+        <SheetSelector
+            filePath={filePath}
+            onSheetSelected={
+                sheets =>
+                {
+                    navigate(`/generate?file=${encodeURIComponent(filePath)}&sheets=${encodeURIComponent(JSON.stringify(sheets))}`);
+                }
+            }
+            onCancel={() => navigate("/")}
+        />
     );
 }
